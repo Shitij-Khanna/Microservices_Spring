f31cdff68bf5c826faab8b3a875476a4f92f99e0
NEWBOOKS\NLshikha <shitij.khanna@eci.nl>
2020-01-30 09:56:31 +0100

    Added security config for microservice url, and added encryption for password

---
+++ Users/src/main/java/com/microservice/users/security/WebSecurity.java
@@ -0,0 +1,32 @@
+package com.microservice.users.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		http.authorizeRequests().antMatchers("/users/**").permitAll();
+		http.headers().frameOptions().disable();
+	}
+
+//	private AuthenticationFilter getAuthenticationFilter() throws Exception
+//	{
+//		AuthenticationFilter authenticationFilter = new AuthenticationFilter(usersService, environment, authenticationManager());
+//		//authenticationFilter.setAuthenticationManager(authenticationManager()); 
+//		authenticationFilter.setFilterProcessesUrl(environment.getProperty("login.url.path"));
+//		return authenticationFilter;
+//	}
+//	
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.userDetailsService(usersService).passwordEncoder(bCryptPasswordEncoder);
+//    }
+
+}
--- Users/src/main/java/com/microservice/users/service/UsersServiceImpl.java
+++ Users/src/main/java/com/microservice/users/service/UsersServiceImpl.java
@@ -2,15 +2,40 @@ package com.microservice.users.service;
 
 import java.util.UUID;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.microservice.users.UsersRepository;
+import com.microservice.users.data.UserEntity;
 import com.microservice.users.shared.UserDto;
 
-public class UsersServiceImpl implements UsersService{
+@Service
+public class UsersServiceImpl implements UsersService {
+
+	UsersRepository usersRepository;
+
+	BCryptPasswordEncoder bCryptPasswordEncoder;
+
+	@Autowired
+	public UsersServiceImpl(UsersRepository usersRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+		this.usersRepository = usersRepository;
+		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+	}
 
 	@Override
 	public UserDto createUser(UserDto userDetails) {
-		// TODO Auto-generated method stub
+		userDetails.setEncryptedPassword(bCryptPasswordEncoder.encode(userDetails.getPassword()));
 		userDetails.setUserId(UUID.randomUUID().toString());
-		return null;
+		ModelMapper modelMapper = new ModelMapper();
+		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+		UserEntity userEntity = modelMapper.map(userDetails, UserEntity.class);
+		usersRepository.save(userEntity);
+		UserDto returnValue = modelMapper.map(userEntity, UserDto.class);
+
+		return returnValue;
 	}
 
 }
