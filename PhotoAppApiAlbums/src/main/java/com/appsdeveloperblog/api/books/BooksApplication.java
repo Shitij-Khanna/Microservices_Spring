bbb08bbc021ae8bb0d2f9ea54fa626a56d1547d1
Shitij Khanna <Shitij.Khanna@bookspot.nl>
2020-02-17 20:16:22 +0100

    Redis cache implementation

--- PhotoAppApiAlbums/src/main/java/com/appsdeveloperblog/api/books/BooksApplication.java
+++ PhotoAppApiAlbums/src/main/java/com/appsdeveloperblog/api/books/BooksApplication.java
@@ -4,15 +4,32 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import com.appsdeveloperblog.api.books.data.Book;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableCaching
+@EnableAspectJAutoProxy
 public class BooksApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BooksApplication.class, args);
 	}
 
-}
+	@Bean
+	JedisConnectionFactory jedisConnectionFactory() {
+		return new JedisConnectionFactory();
+	}
 
+	@Bean
+	RedisTemplate<String, Book> redisTemplate() {
+		RedisTemplate<String, Book> redisTemplate = new RedisTemplate<String, Book>();
+		redisTemplate.setConnectionFactory(jedisConnectionFactory());
+		return redisTemplate;
+	}
+}
--- PhotoAppApiAlbums/src/main/java/com/appsdeveloperblog/api/books/configuration/AspectsBooksMicroservice.java
+++ PhotoAppApiAlbums/src/main/java/com/appsdeveloperblog/api/books/configuration/AspectsBooksMicroservice.java
@@ -9,9 +9,56 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
+import com.appsdeveloperblog.api.books.io.controllers.BookController;
+
 @Aspect
 @Component
 public class AspectsBooksMicroservice {
+
+	// Commented the @before and @after notations for the methods down below because
+	// we do not want to call all these methods from various cases. They are just
+	// for reference and show different possibilities
+
+//	@Pointcut("execution(* com.appsdeveloperblog.api.books.io.controllers.BookController.*(..))")
+	public void allMethodsOfBooksController() {
+		// This method defines a pointcut with the name , which is the name of the
+		// method, i.e. allMethodsOfBooksController
+		// The pointcut is defined to be executed on all methods of BookController
+		// class, as per the definition of the pointcut in the annotation
+		// Whenever we want to create an advice on this this pointcut, we can just refer
+		// to this method , i.e. allMethodsOfBooksController() in the @before / @after
+		// or
+		// whatever annotation we use in the advice
+	}
+
+//	@Pointcut("within(com.appsdeveloperblog.api.books.io.controllers.BookController)")
+	public void allMethodsOfBooksControllerUsingWithin() {
+		// This method defines a pointcut with the name , which is the name of the
+		// method, i.e. allMethodsOfBooksControllerUsingWithin
+		// The pointcut is defined to be executed on all methods of BookController
+		// class, as per the definition of the pointcut in the annotation
+
+		// using within makes it easier to understand that all methods within this class
+		// have been selected in the pointcut, rather than using execution
+	}
+
+//	@Pointcut("execution(* com.appsdeveloperblog.api.books..*)")
+	public void allClassesOfBooksMicroservice() {
+		// The pointcut is defined to be executed on all classes in
+		// all the packages under com.appsdeveloperblog.api.books
+
+		// .. means 0 or more packages inside books
+
+	}
+
+	// args can take classname or interfaces names as
+	// parameters to denote what type
+	// of arguments the pointcut has to expect
+//	@Pointcut("args(com.appsdeveloperblog.api.books.data.Book)")
+	public void methodsWithArgumentBook() {
+		// The pointcut is defined to be executed on all methods which take Book class /
+		// interface as a paramter
+	}
 
 	// This aspect is added to add debug log for invocation of every method on
 	// BooksController
@@ -31,9 +78,75 @@ public class AspectsBooksMicroservice {
 
 	// This aspect is added to add debug logging to show what has been returned
 	// after execution of every method on BooksController
+	// execution
+	// -- first * notates the return type, so it means methods of any return type
+	// -- BookController.* means all methods of BookController class and (.. means
+	// with 0 or more arguments)
 	@AfterReturning(pointcut = "execution(* com.appsdeveloperblog.api.books.io.controllers.BookController.*(..))", returning = "result")
 	public void afterReturning(JoinPoint joinPoint, Object result) {
 		System.out.println("Result is : " + result);
+//		System.out.println("JoinPoint" + joinPoint.getTarget()); 
+		BookController controller = (BookController) joinPoint.getTarget();
+		controller.getBooks(); // gettarget will get the target which we can use to perform other actions
+	}
+
+	// This aspect is added to add debug log for invocation of every method on
+	// BooksController
+//	@Before("allMethodsOfBooksController() && methodsWithArgumentBook()")
+	public void beforeMethodsOfBookControllerWithArgumentBooks(JoinPoint joinPoint) {
+		System.out.print("AOP advice --  Method invocation started : " + joinPoint.getSignature().getName() + "-");
+		System.out.println("Arguments : " + Arrays.toString(joinPoint.getArgs()));
+	}
+
+	// Executed before all public getter methods - notated by get*, of any return
+	// type ,
+	// notated by return type *, but without any arguments because there is no
+	// argument in the method notation
+//	@Before("execution(public * get*())")
+	public void beforePublicGetterMethodsWithNoArgs(JoinPoint joinPoint, Object result) {
+		System.out.println(
+				"AOP advice -- Getter Method invocation started : " + joinPoint.getSignature().getName() + "-");
+	}
+
+	// Executed before all getter methods, public or private - notated by get*, of
+	// any return type , but without any arguments because there is no argument in
+	// the method notation
+	// notated by return type *
+//	@Before("execution(public * get*())")
+	public void beforeAllGetterMethodsWithNoArgs(JoinPoint joinPoint, Object result) {
+		System.out.println(
+				"AOP advice -- Getter Method invocation started : " + joinPoint.getSignature().getName() + "-");
+	}
+
+	// Executed before all getter methods, public or private - notated by get*, of
+	// any return type ,
+	// notated by return type * // get*(..) notates getter methods with 0 or more
+	// arguments
+//	 @Before("execution(public * get*(..))")
+	public void beforeAllGetterMethodsWithArgs(JoinPoint joinPoint, Object result) {
+		System.out.println(
+				"AOP advice -- Getter Method invocation started : " + joinPoint.getSignature().getName() + "-");
+		System.out.println("Arguments : " + Arrays.toString(joinPoint.getArgs()));
+	}
+
+	// Executed before all getter methods, public or private - notated by get*, of
+	// any return type ,
+	// notated by return type * // get*(*) notates getter methods with 1 or more
+	// arguments
+//	 @Before("execution(public * get*(*))")
+	public void beforeAllGetterMethodsWithOneOrMoreArgs(JoinPoint joinPoint, Object result) {
+		System.out.println(
+				"AOP advice -- Getter Method invocation started : " + joinPoint.getSignature().getName() + "-");
+		System.out.println("Arguments : " + Arrays.toString(joinPoint.getArgs()));
+	}
+
+	// This aspect is added to add debug log for invocation of every method on
+	// the classes in this package
+	// @Before("execution(*
+	// com.appsdeveloperblog.api.books.io.controllers.*.*(..))")
+	public void beforeAllMethods(JoinPoint joinPoint) {
+		System.out.print("AOP advice --  Method invocation started : " + joinPoint.getSignature().getName() + "-");
+		System.out.println("Arguments : " + Arrays.toString(joinPoint.getArgs()));
 	}
 
 }
